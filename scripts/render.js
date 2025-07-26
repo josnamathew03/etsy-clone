1dc218b7c6f18639784d5733ac1077a9369d6d75
Josna Mathew <150210764+josnamathew03@users.noreply.github.com>
2025-07-26 14:07:00 +0530

    feat

---
+++ scripts/render.js
@@ -0,0 +1,11 @@
+import { loadProducts , products} from "./products.js";
+
+loadProducts().then(()=>{
+    function renderProductss(){
+        let productshtml=''
+
+        products.forEach((product)=>{
+            productshtml += ``
+        })
+    }
+})
